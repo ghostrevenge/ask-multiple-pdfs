719895292b563d66e1a89eeb86e1276af764c262
Ghost <mostafadiab16@gmail.com>
2023-11-12 02:30:36 +0200

    t

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#import numpy as np
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
@@ -8,7 +9,9 @@ from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
-from langchain.llms import HuggingFaceHub
+
+
+# from langchain.llms import HuggingFaceHub
 
 def get_pdf_text(pdf_docs):
     text = ""
